e700de055643f1cebca5dbe2de57cbc4185dc21c
Boris Tane <boris.tane@gmail.com>
2022-05-14 16:07:59 +0100

    [feat] add refresh command (coming soon)

---
+++ src/commands/refresh.ts
@@ -0,0 +1,38 @@
+import { Arguments, CommandBuilder } from "yargs";
+
+import { BaseOptions, baseOptions, printError } from "../shared";
+
+export interface Options extends BaseOptions {
+  config?: string;
+}
+
+export const command = "refresh";
+export const desc = "Update the state to match remote systems";
+
+export const builder: CommandBuilder<Options, Options> = (yargs) => {
+  return yargs
+    .options({
+      ...baseOptions,
+      config: {
+        type: "string",
+        desc: "The configuration file to execute",
+        alias: "c",
+        default: ".baselime.yml",
+      },
+    })
+    .example([
+      [`
+      $0 refresh
+      $0 refresh --config .baselime.yml --profile prod`,
+      ],
+    ])
+    .fail((_, err, yargs) => {
+      printError(err, yargs);
+    });
+};
+
+export async function handler(argv: Arguments<Options>) {
+  const { config, profile } = argv;
+  console.log("Coming soon.")
+}
+
