b51dbecb6ac5a7ba0e656447cf8b1d2566be3b3e
Boris Tane <boris.tane@gmail.com>
2022-05-14 15:55:18 +0100

    [feat] better help and version descriptions

--- src/cli.ts
+++ src/cli.ts
@@ -3,9 +3,15 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
+const version = () => { return require('../package').version; }
+
 yargs(hideBin(process.argv))
   .commandDir("commands")
   .demandCommand()
   .recommendCommands()
+  .wrap(yargs.terminalWidth())
+  .help("help", "Show this help output, or the help for a specified command or subcommand")
+  .version("version", "Show the current Baselime CLI version", version())
   .strict()
-  .alias({ h: "help" }).argv;
+  .alias({ h: "help" })
+  .argv;
