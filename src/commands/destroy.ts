d2eb52fac2f590b96fd3b73fa4fea21aa38b13fb
Boris Tane <boris.tane@gmail.com>
2022-05-14 16:06:41 +0100

    [feat] add destroy command (coming soon)

---
+++ src/commands/destroy.ts
@@ -0,0 +1,38 @@
+import { Arguments, CommandBuilder } from "yargs";
+
+import { BaseOptions, baseOptions, printError } from "../shared";
+
+export interface Options extends BaseOptions {
+  config?: string;
+}
+
+export const command = "destroy";
+export const desc = "Destroy previously-created observability configurations";
+
+export const builder: CommandBuilder<Options, Options> = (yargs) => {
+  return yargs
+    .options({
+      ...baseOptions,
+      config: {
+        type: "string",
+        desc: "The configuration file to execute",
+        alias: "c",
+        default: ".baselime.yml",
+      },
+    })
+    .example([
+      [`
+      $0 destroy
+      $0 destroy --config .baselime.yml --profile prod`,
+      ],
+    ])
+    .fail((_, err, yargs) => {
+      printError(err, yargs);
+    });
+};
+
+export async function handler(argv: Arguments<Options>) {
+  const { config, profile } = argv;
+  console.log("Coming soon.")
+}
+
