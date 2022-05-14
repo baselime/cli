0a254fa7671becb79b5c95f39a38fb6ceb12cdaa
Boris Tane <boris.tane@gmail.com>
2022-05-14 16:04:52 +0100

    [feat] add plan command (coming soon)

---
+++ src/commands/plan.ts
@@ -0,0 +1,38 @@
+import { Arguments, CommandBuilder } from "yargs";
+
+import { BaseOptions, baseOptions, printError } from "../shared";
+
+export interface Options extends BaseOptions {
+  config?: string;
+}
+
+export const command = "plan";
+export const desc = "Show changes required by the current configuration";
+
+export const builder: CommandBuilder<Options, Options> = (yargs) => {
+  return yargs
+    .options({
+      ...baseOptions,
+      config: {
+        type: "string",
+        desc: "The configuration file to execute",
+        alias: "c",
+        default: ".baselime.yml",
+      },
+    })
+    .example([
+      [`
+      $0 plan
+      $0 plan --config .baselime.yml --profile prod`,
+      ],
+    ])
+    .fail((_, err, yargs) => {
+      printError(err, yargs);
+    });
+};
+
+export async function handler(argv: Arguments<Options>) {
+  const { config, profile } = argv;
+  console.log("Coming soon.")
+}
+
