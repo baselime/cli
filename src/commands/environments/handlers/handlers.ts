4499baaa053901761f1ffc2b0f63b295a099cb4f
Boris Tane <boris.tane@gmail.com>
2022-06-17 09:48:36 +0100

    [chore] add a random string in the stack-name when generating a cloudformation file

--- src/commands/environments/handlers/handlers.ts
+++ src/commands/environments/handlers/handlers.ts
@@ -3,12 +3,15 @@ import api from "../../../services/api/api";
 import spinner from "../../../services/spinner";
 import { OutputFormat } from "../../../shared";
 import https from "https";
+import chalk from "chalk";
 
 async function setup(format: OutputFormat, type: string, params: { account: string, region: string }, alias: string, otp: string) {
   const s = spinner.get();
   if (type !== "aws") {
     return console.log("Baselime currently only supports environments on AWS");
   }
+
+    const randomString = Math.random().toString(20).substring(2, 8);
 
   s.start("Generating your CloudFormation template");
   const { url } = await api.cfTeamplateGenerate({
@@ -20,16 +23,16 @@ async function setup(format: OutputFormat, type: string, params: { account: stri
 
   const filename = `baselime-environment-${params.account}-${params.region}.yaml`;
   const file = createWriteStream(filename);
-  const request = https.get(url, function (response) {
+  https.get(url, function (response) {
     response.pipe(file);
 
     file.on("finish", () => {
       file.close();
       s.succeed(`Template Generated ${process.cwd()}/${filename}`);
-      console.log("Run the following command to connect your AWS account to Baselime");
+      console.log(chalk.bold("Run the following command to connect your AWS account to Baselime"));
       console.log(`Make sure you are using the AWS credentials for the account ${params.account} in ${params.region}`);
       console.log(`aws cloudformation create-stack \\
-      --stack-name baselime-environment \\
+      --stack-name baselime-environment-${randomString} \\
       --capabilities CAPABILITY_NAMED_IAM \\
       --template-body file://${process.cwd()}/${filename}`);
     });
