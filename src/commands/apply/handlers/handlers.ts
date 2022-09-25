e69ce08fe7ed186a19fd1a1f7691cbcb83ec9b10
Boris Tane <boris.tane@gmail.com>
2022-09-25 22:21:55 +0100

    [feat] give feedback to developers on the status of the `apply`

--- src/commands/apply/handlers/handlers.ts
+++ src/commands/apply/handlers/handlers.ts
@@ -6,6 +6,10 @@ import { readFileSync } from "fs";
 import { getVersion, writeOutFile } from "../../../shared";
 import { verifyPlan } from "../../plan/handlers";
 import * as prompts from "./prompts";
+import { Deployment, DeploymentStatus } from "../../../services/api/paths/deployments";
+import { promisify } from "util";
+
+const wait = promisify(setTimeout);
 
 async function apply(config: string, skip: boolean = false) {
   const s = spinner.get();
@@ -14,21 +18,35 @@ async function apply(config: string, skip: boolean = false) {
   await verifyPlan(metadata, resources, false);
 
   const res = skip ? true : await prompts.promptApply();
-  
+
   if (!res) {
     process.exit(0);
   }
 
   writeOutFile(config, metadata, resources);
-  s.start("Checking submission status...");
+  s.start("Submitting the plan to the baselime backend...");
   const { url, id } = await api.uploadUrlGet(metadata.application, getVersion());
   const data = readFileSync(`${config}/.out/.baselime.json`, "utf-8").toString();
   await api.uplaod(url, data);
-  s.succeed(
-    `Submitted your observability configurations. id: ${chalk.bold(
-      chalk.cyan(id),
-    )}`,
-  );
+  s.start("Checking apply status...");
+
+
+  let isComplete = false;
+  let deployment: Deployment | undefined = undefined;
+  let count = 0;
+  const maxCheck = 20;
+  while (!isComplete && count < maxCheck) {
+    await wait(800);
+    deployment = await api.deploymentGet(metadata.application, id);
+    isComplete = deployment.status != DeploymentStatus.IN_PROGRESS;
+    console.log(`\nStatus: ${chalk.bold(deployment.status)}`);
+    count += 1;
+  }
+  if (deployment?.status === DeploymentStatus.SUCCESS) {
+    s.succeed(`Successfully applied an observability plan: ${chalk.bold(chalk.greenBright(id),)}`);
+    return;
+  }
+  s.fail(`Failed to apply an observability plan: ${chalk.bold(chalk.redBright(id),)}`);
 }
 
 async function validate(folder: string) {
