c152b83fd07b37f7548c02edf9f00c0e18e3540b
Boris Tane <boris.tane@gmail.com>
2022-11-30 02:24:58 +0000

    [feat] add query command

--- src/commands/queries/handlers/handlers.ts
+++ src/commands/queries/handlers/handlers.ts
@@ -2,7 +2,6 @@ import spinner from "../../../services/spinner/index";
 import api from "../../../services/api/api";
 import outputs from "./outputs";
 import { OutputFormat } from "../../../shared";
-import { getTimeframe } from "../../../services/timeframes/timeframes";
 import dayjs from "dayjs";
 import chalk from "chalk";
 const utc = require('dayjs/plugin/utc')
@@ -16,24 +15,6 @@ async function list(format: OutputFormat, application?: string) {
   outputs.list(queries, format);
 }
 
-async function createRun(format: OutputFormat, from: string, to: string, application: string, id: string) {
-  const s = spinner.get();
-  const timeframe = getTimeframe(from, to);
-  const f = dayjs.utc(timeframe.from);
-  const t = dayjs.utc(timeframe.to);
-  const timeFormat = f.isSame(t, "day") ? "HH:mm:ss" : "YYYY-MM-DDTHH:mm:ss";
-  s.start(`Running the query from ${chalk.bold(f.format(timeFormat))} to ${chalk.bold(t.format(timeFormat))} [UTC]`);
-
-  const { queryRun, calculations: { aggregates, series } } = await api.queryRunCreate({
-    application,
-    queryId: id,
-    timeframe,
-  });
-  s.succeed();
-  outputs.getQueryRun(queryRun, aggregates, series, [], format);
-}
-
 export default {
   list,
-  createRun,
 };
--- src/commands/queries/handlers/outputs.ts
+++ src/commands/queries/handlers/outputs.ts
@@ -24,50 +24,6 @@ function list(queries: Query[], format: OutputFormat) {
   console.log(`✨ ${chalk.bold(chalk.cyan(`${queries.length} queries`))}`);
 }
 
-function getQueryRun(queryRun: QueryRun, aggregates: Record<string, number | Record<string, number>>, series: Series[], events: Event[], format: OutputFormat) {
-  if (format === "json") {
-    console.log(JSON.stringify({ queryRun, aggregates, series, events }, null, 4));
-    return;
-  }
-  const runTable = new Table({
-    chars: tableChars,
-    head: ["Id", "QueryId", "From", "To", "Status", "Created"].map((e) => `${chalk.bold(chalk.cyan(e))}`),
-  });
-
-  runTable.push([queryRun.id, queryRun.query.id, `${dayjs(queryRun.timeframe.from).format()}`, `${dayjs(queryRun.timeframe.to).format()}`, queryRun.status, queryRun.created]);
-
-  let aggregatesTable: Table.Table;
-  const isGrouped = typeof aggregates._count !== "number";
-  if (isGrouped) {
-    const groups = Object.keys(aggregates._count);
-    const calculationKeys = Object.keys(aggregates).filter(k => k !== "_count")
-    aggregatesTable = new Table({
-      chars: tableChars,
-      head: ["", ...calculationKeys].map((e) => `${chalk.bold(chalk.cyan(e))}`),
-    });
-
-    groups.forEach(group => {
-      const vals = calculationKeys.map(key => (aggregates as Record<string, Record<string, number>>)[key][group])
-      aggregatesTable.push([group, ...vals]);
-    });
-  } else {
-    aggregatesTable = new Table({
-      chars: tableChars,
-      head: ["Aggregate", "Value"].map((e) => `${chalk.bold(chalk.cyan(e))}`),
-    });
-
-    Object.keys(aggregates).forEach((key: string) => {
-      if (key === "_count") return;
-      aggregatesTable.push([key, (aggregates as Record<string, number>)[key]]);
-    });
-  }
-
-  console.log(runTable.toString());
-  console.log(aggregatesTable.toString());
-  console.log(`Follow this url: https://console.${BASELIME_DOMAIN}/${queryRun.workspaceId}/${queryRun.environmentId}/${queryRun.application}/queries/${queryRun.query.id}/${queryRun.id}`)
-}
-
 export default {
   list,
-  getQueryRun,
 };
