4eca06e49bd632501bc69f513fb600d4aedd35b8
Boris Tane <boris.tane@gmail.com>
2022-12-01 09:09:48 +0000

    [feat] add "-" to query filter regex

--- src/commands/push/handlers/checks.ts
+++ src/commands/push/handlers/checks.ts
@@ -12,7 +12,7 @@ const namespaceCombinations = ["INCLUDE", "EXCLUDE", "STARTS_WITH"];
 const channelTypes = ["slack", "webhook"];
 const groupByTypes = ["string", "number", "boolean"];
 
-const queryFilterRegex = new RegExp("^([\\w.@]+)\\s(" + operations.join("|") + ")\\s'?(.*?)'?$");
+const queryFilterRegex = new RegExp("^([\\w.@-]+)\\s(" + operations.join("|") + ")\\s'?(.*?)'?$");
 const alertThresholdRegex = new RegExp("^(" + operations.filter(o => !["INCLUDES", "IN", "NOT_IN"].includes(o)).join("|") + ")\\s([0-9]*)$");
 const idRegex = /^[a-zA-Z0-9-_]+$/;
 
