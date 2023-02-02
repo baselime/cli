a184636cfdbf73107df1202cecc467ce150c204e
Boris Tane <boris.tane@gmail.com>
2023-02-02 18:50:54 +0000

    v0.0.51

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [Unreleased]
+
+## [0.0.51] 2023-02-02
+
+### Added
+
+- Add ca-central-1 to the supported regions
+
 ## [0.0.50] 2023-02-02
 
 - Nicely format templates when calling `baselime templates get`
--- src/commands/auth/handlers/prompts.ts
+++ src/commands/auth/handlers/prompts.ts
@@ -49,7 +49,7 @@ export async function promptAWSAccountId(): Promise<string> {
 }
 
 export async function promptAWSRegion(): Promise<string> {
-  const regions = ["us-east-1", "us-east-2", "us-west-2", "eu-central-1", "eu-west-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "eu-west-1"];
+  const regions = ["us-east-1", "us-east-2", "us-west-2", "eu-central-1", "eu-west-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "eu-west-1", "ca-central-1"];
 
   const { region } = await prompt<{ region: string }>({
     type: "autocomplete",
